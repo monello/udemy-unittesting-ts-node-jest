9c460d91550fcb8f61268adc1aef5a101544b437
monello <louw.morne@gmail.com>
2023-02-07 17:21:13 +0200

    test: s2v7 - toBe() vs toEqual()

--- src/tests/Utils.test.ts
+++ src/tests/Utils.test.ts
@@ -5,4 +5,20 @@ describe('Utils test suite', () => {
         const result = Utils.toUpperCase('abc');
         expect(result).toBe('ABC');
     });
+
+    it('parses simple URL', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login');
+        expect(parsedUrl.href).toBe('http://localhost:8080/login');
+        expect(parsedUrl.port).toBe('8080');
+        expect(parsedUrl.protocol).toBe('http:');
+        expect(parsedUrl.query).toEqual({});
+    });
+
+    it('parses URL with query', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login?username=user&password=secret');
+        expect(parsedUrl.query).toEqual({
+            username: 'user',
+            password: 'secret'
+        });
+    });
 });
