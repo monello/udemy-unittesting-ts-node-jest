271af77068fe2cec90cbd34a7e8d8622ae463b58
monello <louw.morne@gmail.com>
2023-02-22 06:17:09 +0200

    test: s2v9 - updated my jest config to say which files to include in coverage

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,7 @@ module.exports = {
     },
     testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    verbose: true
-}
+    verbose: true,
+    collectCoverage: true,  // Here we turn on coverage
+    collectCoverageFrom: ['<rootDir>/src/app/**/*.ts'] // Here we tell jest to look for all files that that end with '.ts' inside the 'src/app' folder and all it's sub-folders
+};
