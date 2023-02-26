fbf7afcd9140513d73a83b84d8504073e87ba082
monello <louw.morne@gmail.com>
2023-02-26 23:21:47 +0200

    test: a cleaner way to write tests - split into smaller tests + test suites

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -14,38 +14,36 @@ describe('Utils test suite', () => {
         expect(actual).toBe(expected);
     });
 
-    it('should return info for a valid string', () => {
-        // ARRANGE
-        const sut = getStringInfo;
-        const expected: stringInfo = {
-            lowerCase: 'my-string',
-            upperCase: 'MY-STRING',
-            characters: ['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g'],
-            length: 9,
-            extraInfo: {}
-        };
+    describe('getStringInfo for arg My-String should', () => {
+        test('return right length', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.characters).toHaveLength(9);
+        });
+        test('return right lower case', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.lowerCase).toBe('my-string');
+        });
+        test('return right upper case', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.upperCase).toBe('MY-STRING');
+        });
+        test('return right characters', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g']);
+            expect(actual.characters).toContain<string>('M');
+            expect(actual.characters).toEqual(
+                expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y', '-'])
+            );
+        });
+        test('return defined extra info', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.extraInfo).toBeDefined();
+        });
 
-        // ACT
-        const actual = sut('My-String');
-
-        // ASSERT
-        expect(actual.lowerCase).toBe(expected.lowerCase);
-        expect(actual.upperCase).toBe(expected.upperCase);
-        expect(actual.characters).toEqual(expected.characters); // note use of toEqual to compare array
-        expect(actual.characters).toContain('M'); // handy matcher to check if an array contains an expected element
-        expect(actual.characters).toContain<string>('M'); // using the optional Generic to apply more type-safety
-        // this next example is usefuly when you don't know what the order of the array elements will be when returned, but you do know what st should contain
-        expect(actual.characters).toEqual(
-            expect.arrayContaining(['-', 'g', 'i', 'M'])
-        );
-        expect(actual.length).toBe(expected.length);
-        expect(actual).toHaveLength(expected.length); // note the use of the handy tohaveLength matcher
-        expect(actual.extraInfo).toEqual(expected.extraInfo); // note use of toEqual to compare objects
-
-        // ways to assert that something is NOT undefined
-        expect(actual.extraInfo).not.toBe(undefined);
-        expect(actual.extraInfo).not.toBeUndefined();
-        expect(actual.extraInfo).toBeDefined();
-        expect(actual.extraInfo).toBeTruthy();
+        test('return right extra info', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.extraInfo).toEqual({});
+        });
     });
+
 });
