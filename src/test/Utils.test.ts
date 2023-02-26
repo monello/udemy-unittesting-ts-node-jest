83913b0df38a228969b68feab542b1a0faabae90
monello <louw.morne@gmail.com>
2023-02-26 23:15:26 +0200

    test: some basic jest assertions nad matchers

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, stringInfo, toUpperCase } from "../app/Utils";
 
 describe('Utils test suite', () => {
     it('should return uppercase of a valid string', () => {
@@ -8,9 +8,44 @@ describe('Utils test suite', () => {
         const expected = 'ABC';
 
         // ACT
-        const result = sut('abc');
+        const actual = sut('abc');
 
         // ASSERT
-        expect(result).toBe(expected);
+        expect(actual).toBe(expected);
+    });
+
+    it('should return info for a valid string', () => {
+        // ARRANGE
+        const sut = getStringInfo;
+        const expected: stringInfo = {
+            lowerCase: 'my-string',
+            upperCase: 'MY-STRING',
+            characters: ['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g'],
+            length: 9,
+            extraInfo: {}
+        };
+
+        // ACT
+        const actual = sut('My-String');
+
+        // ASSERT
+        expect(actual.lowerCase).toBe(expected.lowerCase);
+        expect(actual.upperCase).toBe(expected.upperCase);
+        expect(actual.characters).toEqual(expected.characters); // note use of toEqual to compare array
+        expect(actual.characters).toContain('M'); // handy matcher to check if an array contains an expected element
+        expect(actual.characters).toContain<string>('M'); // using the optional Generic to apply more type-safety
+        // this next example is usefuly when you don't know what the order of the array elements will be when returned, but you do know what st should contain
+        expect(actual.characters).toEqual(
+            expect.arrayContaining(['-', 'g', 'i', 'M'])
+        );
+        expect(actual.length).toBe(expected.length);
+        expect(actual).toHaveLength(expected.length); // note the use of the handy tohaveLength matcher
+        expect(actual.extraInfo).toEqual(expected.extraInfo); // note use of toEqual to compare objects
+
+        // ways to assert that something is NOT undefined
+        expect(actual.extraInfo).not.toBe(undefined);
+        expect(actual.extraInfo).not.toBeUndefined();
+        expect(actual.extraInfo).toBeDefined();
+        expect(actual.extraInfo).toBeTruthy();
     });
 });
