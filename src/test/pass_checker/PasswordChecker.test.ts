dbcbcf5ba8a34729c90983fe54865c09e3d3cb44
monello <louw.morne@gmail.com>
2023-03-02 06:45:07 +0200

    test: example tdd - difficult to show in a commit

--- src/test/pass_checker/PasswordChecker.test.ts
+++ src/test/pass_checker/PasswordChecker.test.ts
@@ -8,7 +8,75 @@ describe('PasswordChecker test suite', () => {
         sut = new PasswordChecker();
     });
 
-    it('just tesing the setup', () => {
-        sut.checkPassword();
+    describe('password length checks', () => {
+        it('password with < 8 characters is invalid', () => {
+            // ARRANGE
+            const password = '123456a';
+
+            // ACT
+            const actual = sut.checkPassword(password);
+
+            // ASSERT
+            expect(actual).toBeFalsy();
+        });
+
+        it('password with >= 8 characters is okay', () => {
+            // ARRANGE
+            const password = '123456Ab';
+
+            // ACT
+            const actual = sut.checkPassword(password);
+
+            // ASSERT
+            expect(actual).toBeTruthy();
+        });
+    });
+
+    describe('uppercase letter checks', () => {
+        it('password with no uppercase letters is invalid', () => {
+            // ARRANGE
+            const password = '1234abcd';
+
+            // ACT
+            const actual = sut.checkPassword(password);
+
+            // ASSERT
+            expect(actual).toBeFalsy();
+        });
+
+        it('password with at least 1 uppercase letter is okay', () => {
+            // ARRANGE
+            const password = '1234abcX';
+
+            // ACT
+            const actual = sut.checkPassword(password);
+
+            // ASSERT
+            expect(actual).toBeTruthy();
+        });
+    });
+
+    describe('lowercase letter checks', () => {
+        it('password with no lowercase letters is invalid', () => {
+            // ARRANGE
+            const password = '1234ABCD';
+
+            // ACT
+            const actual = sut.checkPassword(password);
+
+            // ASSERT
+            expect(actual).toBeFalsy();
+        });
+
+        it('password with at least 1 lowercase letter is okay', () => {
+            // ARRANGE
+            const password = '1234ABCd';
+
+            // ACT
+            const actual = sut.checkPassword(password);
+
+            // ASSERT
+            expect(actual).toBeTruthy();
+        });
     });
 });
